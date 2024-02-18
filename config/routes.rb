d9fc567ef7daa389cc922498021b7517506e7596
Gor <7495500@gmail.com>
2024-02-18 12:37:39 +0300

    Add basic app configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'posts#index'
+  get 'posts/:id/view_count', to: 'posts#view_count', as: 'view_count'
+  resources :posts, except: :index
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+1.upto(10) do |index|
+  post = Post.create(
+    title: "Post #{index}",
+    body: "post body #{index}",
+    views: rand(100)
+  )
+  puts post.title
+end
